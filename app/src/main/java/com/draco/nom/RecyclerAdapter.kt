9a33e4f39a9a4f95930723e30e40210990cf744e
Tyler Nijmeh <tylernij@gmail.com>
2020-08-16 13:55:13 -0700

    Remove unused imports
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- app/src/main/java/com/draco/nom/RecyclerAdapter.kt
+++ app/src/main/java/com/draco/nom/RecyclerAdapter.kt
@@ -3,17 +3,14 @@ package com.draco.nom
 import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.hardware.display.DisplayManager
 import android.net.Uri
-import android.os.Build
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.app.NotificationCompat
