686d1f579795c9866837fb099e15400edaea81a9
tytydraco <tylernij@gmail.com>
2021-01-06 20:05:14 -0800

    Switch to fullscreen mode
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/nom/views/MainActivity.kt
+++ app/src/main/java/com/draco/nom/views/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.draco.nom.views
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
@@ -23,6 +27,12 @@ class MainActivity: AppCompatActivity() {
             recyclerAdapter.notifyDataSetChanged()
         }
 
+        window.decorView.setOnApplyWindowInsetsListener { view, windowInsets ->
+            view.post { immersive() }
+            return@setOnApplyWindowInsetsListener windowInsets
+        }
+
+
         recyclerAdapter = LauncherRecyclerAdapter(this, emptyArray()).apply {
             setHasStableIds(true)
         }
@@ -33,6 +43,23 @@ class MainActivity: AppCompatActivity() {
             it.layoutManager = GridLayoutManager(this, columns)
             it.edgeEffectFactory = RecyclerEdgeEffectFactory()
             it.setItemViewCacheSize(1000)
+        }
+    }
+
+    private fun immersive() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            with (window.insetsController!!) {
+                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN
         }
     }
 
