86bea828ff65535fcf6f4abeee8c5512d7dcc288
tytydraco <tylernij@gmail.com>
2021-03-24 10:54:27 -0700

    Load icons asynchronously
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/nom/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/draco/nom/viewmodels/MainActivityViewModel.kt
@@ -11,9 +11,13 @@ import androidx.core.hardware.display.DisplayManagerCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.draco.nom.R
 import com.draco.nom.models.AppInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.*
+import kotlin.coroutines.coroutineContext
 
 class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
     private val context = application.applicationContext
@@ -26,31 +30,33 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
     }
 
     fun updateList() {
-        val launcherIntent = Intent(Intent.ACTION_MAIN, null).apply {
-            addCategory(Intent.CATEGORY_LAUNCHER)
-        }
+        viewModelScope.launch(Dispatchers.IO) {
+            val launcherIntent = Intent(Intent.ACTION_MAIN, null).apply {
+                addCategory(Intent.CATEGORY_LAUNCHER)
+            }
 
-        val activities = context.packageManager.queryIntentActivities(launcherIntent, 0)
-        val newAppList = arrayListOf<AppInfo>()
+            val activities = context.packageManager.queryIntentActivities(launcherIntent, 0)
+            val newAppList = arrayListOf<AppInfo>()
 
-        for (app in activities) {
-            if (app.activityInfo.packageName == context.packageName)
-                continue
+            for (app in activities) {
+                if (app.activityInfo.packageName == context.packageName)
+                    continue
 
-            newAppList.add(
-                AppInfo(
-                    app.activityInfo.loadLabel(context.packageManager).toString(),
-                    app.activityInfo.packageName
+                newAppList.add(
+                    AppInfo(
+                        app.activityInfo.loadLabel(context.packageManager).toString(),
+                        app.activityInfo.packageName
+                    )
                 )
-            )
-        }
+            }
 
-        newAppList.sortBy {
-            it.label.toLowerCase(Locale.getDefault())
-        }
+            newAppList.sortBy {
+                it.label.toLowerCase(Locale.getDefault())
+            }
 
-        if (!_appList.value.contentEquals(newAppList.toTypedArray()))
-            _appList.value = newAppList.toTypedArray()
+            if (!_appList.value.contentEquals(newAppList.toTypedArray()))
+                _appList.postValue(newAppList.toTypedArray())
+        }
     }
 
     fun getColumns(displayMetrics: DisplayMetrics): Int {
