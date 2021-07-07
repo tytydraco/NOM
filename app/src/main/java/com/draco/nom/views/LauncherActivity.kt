24558daec33ceab846c8b33bcc9c433014728ebc
Tyler Nijmeh <tylernij@gmail.com>
2021-07-07 01:25:11 -0700

    Revert "Add swipe to refresh"
    This reverts commit 7d490d7a5717c1545cf96aae38c7056b7e25731d.

--- app/src/main/java/com/draco/nom/views/LauncherActivity.kt
+++ app/src/main/java/com/draco/nom/views/LauncherActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.draco.nom.R
 import com.draco.nom.recyclers.LauncherRecyclerAdapter
 import com.draco.nom.recyclers.factories.LauncherEdgeEffectFactory
@@ -18,7 +17,6 @@ class LauncherActivity: AppCompatActivity() {
     private val viewModel: LauncherActivityViewModel by viewModels()
     private lateinit var recyclerAdapter: LauncherRecyclerAdapter
 
-    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private lateinit var recycler: RecyclerView
     private lateinit var progress: ProgressBar
 
@@ -26,7 +24,6 @@ class LauncherActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout)
         recycler = findViewById(R.id.recycler)
         progress = findViewById(R.id.progress)
 
@@ -57,11 +54,6 @@ class LauncherActivity: AppCompatActivity() {
             setHasFixedSize(true)
             setItemViewCacheSize(1000)
             layoutManager!!.isItemPrefetchEnabled = true
-        }
-
-        swipeRefreshLayout.setOnRefreshListener {
-            viewModel.updatePackageIdList()
-            swipeRefreshLayout.isRefreshing = false
         }
     }
 
